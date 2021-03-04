10279b32485cc9e4680e300a671b3d10be001622
unknown <wal.said2017@gmail.com>
2021-03-04 09:53:57 +0100

    First commit

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  db: 'mongodb://localhost:27017/angular8mean'
+ // db: 'mongodb://localhost:27017/angular8mean'
+  db: 'mongodb+srv://test:testtest@cluster0.woms6.mongodb.net/angular8mean?retryWrites=true&w=majority'
 };
